d073641499730dda8810164fb0190f309958d9e9
Ivan <themaster.coronado1234@gmail.com>
2023-07-04 15:13:28 -0700

    Welcome page finished

---
+++ src/Welcome.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Welcome.css'
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import { WelcomeCarousel } from './WelcomeCarousel';
+import logo from './assets/qt=q_95.webp'; // with import
+
+export const Welcome = () => {
+  return (
+    <div className='welcome'>
+
+
+        <WelcomeCarousel />
+    
+
+        <div className='content'> 
+                <img src={logo}/>
+                <p className='content-p'>"El hallazgo afortunado de un buen libro puede cambiar el destino de un alma". Marcel Prévost</p>
+                <Button colorScheme='orange' variant='solid'>
+                        Sobre Nosotros
+                </Button>
+        </div>
+    </div>
+  )
+}
---
+++ src/WelcomeCarousel.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './WelcomeCarousel.css'
+
+export const WelcomeCarousel = () => {
+  return (
+    <Carousel className='carousel' autoPlay showIndicators={false} showStatus={false} interval={2000} showThumbs={false} infiniteLoop showArrows={false}>
+        <div className='background-image image1'>
+        </div>
+        <div className='background-image image2'>
+        </div>
+        <div className='background-image image3'>
+        </div>
+        <div className='background-image image4'>
+        </div>
+        <div className='background-image image5'>
+        </div>
+
+    </Carousel>
+  )
+}
